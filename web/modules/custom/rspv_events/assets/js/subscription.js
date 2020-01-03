43bc0e54d2471019cf1cad25b6e7ba8c1355b551
ericbsi <eu@ericvinicius.com.br>
2020-01-03 03:36:09 -0300

    Updates markup and styles for event full page.

--- web/modules/custom/rspv_events/assets/js/subscription.js
+++ web/modules/custom/rspv_events/assets/js/subscription.js
@@ -4,18 +4,46 @@
   Drupal.behaviors.subscription = {
     attach: function attach(context) {
       const $subscribeButton = $('.node--view-mode-full > .btn-join-event', context);
+      const $participantsList = $('#participants', context);
+      const $unsubscribeButton = $('.node--view-mode-full > .btn-leave-event', context);
+
       if ($subscribeButton.length) {
         $subscribeButton.click(function(){
+
+          $subscribeButton.addClass('disabled');
+          $subscribeButton.text('Subscribing...');
+
           $.ajax({
             url: Drupal.url('event/subscribe/' + $(this).data('event')),
             type:"GET",
             success: function(response) {
-              console.log(response)
+              if(response.error !== undefined){
+                alert("An error has occurred. Try again later.")
+              }
+              else {
+                $subscribeButton
+                  .removeClass('btn-join-event btn-rspv-primary disabled')
+                  .addClass('btn-warning btn-leave-event');
+
+                  $subscribeButton.text('Cancel subscription');
+
+                if($participantsList.length) {
+                  $participantsList.append('<li id="user-'+response.user_id+'">'+response.user_name+'</li>');
+                }
+              }
             }
           });
           return false;
         })
       }
+
+      if ($unsubscribeButton.length) {
+        $unsubscribeButton.click(function(){
+          alert("TOdo")
+          return false;
+        })
+      }
+
     }
   };
 
