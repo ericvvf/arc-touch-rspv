2cf77b20e0f2e0ae232bb556867beb1e97dfef93
ericbsi <eu@ericvinicius.com.br>
2020-01-06 02:03:34 -0300

    Updates subscribe/unsubscribe event workflow.

--- web/modules/custom/rspv_events/assets/js/subscription.js
+++ web/modules/custom/rspv_events/assets/js/subscription.js
@@ -5,13 +5,13 @@
     attach: function attach(context) {
       const $subscribeButton = $('.node--view-mode-full > .btn-subscribe', context);
       const $participantsList = $('#participants', context);
+      const $participantsListWrapper = $('.node--view-mode-full > .participants-wrapper', context);
 
       if ($subscribeButton.length) {
         $subscribeButton.click(function(){
 
           $subscribeButton.addClass('disabled');
           $subscribeButton.text('Wait...');
-
           $.ajax({
             url: Drupal.url('event/subscribe/' + $(this).data('event')),
             type:"GET",
@@ -26,17 +26,31 @@
                   $subscribeButton
                     .removeClass('btn-rspv-primary disabled')
                     .addClass('btn-warning');
-                    $subscribeButton.text('Cancel subscription');
-                    if($participantsList.length) {
-                      $participantsList.append('<li id="user-'+response.user_id+'">'+response.user_name+'</li>');
-                    }
+                  $subscribeButton.text('Cancel subscription');
+                  if($participantsList.length > 0) {
+                    $participantsList.append('<li id="user-'+response.user_id+'">'+response.user_name+'</li>');
+                  }
+                  else {
+                    console.log("2",$participantsList.length)
+                    $participantsListWrapper.append(''+
+                    '<h3>List of attenders</h3>' +
+                    '<ul id="participants">' +
+                    '<li id="user-'+response.user_id+'">'+response.user_name+'</li>' +
+                    '</ul>'
+                    +'')
+                  }
                 }
                 else if ($action === 'remove') {
                   $subscribeButton
-                  .removeClass('btn-warning disabled')
-                  .addClass('btn-rspv-primary');
+                    .removeClass('btn-warning disabled')
+                    .addClass('btn-rspv-primary');
                   $subscribeButton.text('Join the event');
-                  $('#user-'+response.user_id).remove();
+                  if ($participantsList.find('li').length < 1) {
+                    $participantsListWrapper.html('');
+                  }
+                  else {
+                    $('#user-'+response.user_id).remove();
+                  }
                 }
               }
             }
